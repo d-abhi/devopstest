8e7f3bcb1fbb286d65b8339bb51c7cd784d10ec0
d-abhi <133018920+d-abhi@users.noreply.github.com>
2024-08-10 04:03:54 +0530

    Delete start_container.sh

--- start_container.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -e
-
-# Pull the Docker image from Docker Hub
-docker pull abhid92/simple-python-flask-app
-
-# Run the Docker image as a container
-docker run -d -p 5000:5000 abhid92/simple-python-flask-app
-
